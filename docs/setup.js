b97652d9c0f45326a7b2cdbfb05a8dfe66f93e11
ytyaru <ytyaru@outlook.jp>
2018-11-02 10:33:16 +0900

    電源電圧の算出

--- docs/setup.js
+++ docs/setup.js
@@ -1,3 +1,24 @@
 window.addEventListener('DOMContentLoaded', function() {
-	document.forms.SourceForm.SourceVoltage.focus();
+	SetupSourceForm();
 });
+function SetupSourceForm() {
+	document.forms.SourceForm.SourceVoltage.focus();
+	var v = document.getElementById("SourceVoltage");
+	v.addEventListener("change", CalcAllSourceVoltage);
+	v.addEventListener("keyup", CalcAllSourceVoltage);
+	v.addEventListener("paste", CalcAllSourceVoltage);
+	v.addEventListener("input", CalcAllSourceVoltage);
+	var n = document.getElementById("SourceNumber");
+	n.addEventListener("change", CalcAllSourceVoltage);
+	n.addEventListener("keyup", CalcAllSourceVoltage);
+	n.addEventListener("paste", CalcAllSourceVoltage);
+	n.addEventListener("input", CalcAllSourceVoltage);
+	CalcAllSourceVoltage();
+}
+function CalcAllSourceVoltage() {
+	var allSourceVoltage = 
+		document.forms.SourceForm.SourceVoltage.value * 
+		document.forms.SourceForm.SourceNumber.value;
+	var target = document.getElementById("AllSourceVoltage");
+	target.value = allSourceVoltage;
+}
