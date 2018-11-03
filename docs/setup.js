de0893c5b593ea8fc7440edb8b7ca0529a309936
ytyaru <ytyaru@outlook.jp>
2018-11-03 15:00:04 +0900

    LED電流の算出を追加

--- docs/setup.js
+++ docs/setup.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
 	SetupSourceForm();
 	SetupRegisterForm();
+	SetupLEDForm();
 });
 function SetupSourceForm() {
 	document.forms.SourceForm.SourceVoltage.focus();
@@ -16,12 +17,14 @@ function SetupSourceForm() {
 	n.addEventListener("input", CalcAllSourceVoltage);
 	CalcAllSourceVoltage();
 }
+// 電源電圧を算出する
 function CalcAllSourceVoltage() {
 	var allSourceVoltage = 
 		document.forms.SourceForm.SourceVoltage.value * 
 		document.forms.SourceForm.SourceNumber.value;
 	var target = document.getElementById("AllSourceVoltage");
 	target.value = allSourceVoltage;
+	CalcLEDIntensity();
 }
 function SetupRegisterForm() {
 	var ro = document.getElementById("RegisterOhm");
@@ -36,10 +39,27 @@ function SetupRegisterForm() {
 	re.addEventListener("input", CalcRegisterValue);
 	CalcRegisterValue();
 }
+// 抵抗器の抵抗値を算出する
 function CalcRegisterValue() {
 	var RegisterValue = 
 		document.forms.RegisterForm.RegisterOhm.value * 
 		( 10 ** document.forms.RegisterForm.RegisterExponential.value );
 	var target = document.getElementById("RegisterValue");
 	target.value = RegisterValue;
+	CalcLEDIntensity();
+}
+function SetupLEDForm() {
+	var v = document.getElementById("LEDForwardVoltage");
+	v.addEventListener("change", CalcLEDIntensity);
+	v.addEventListener("keyup", CalcLEDIntensity);
+	v.addEventListener("paste", CalcLEDIntensity);
+	v.addEventListener("input", CalcLEDIntensity);
+}
+// LED電流を算出する
+function CalcLEDIntensity() {
+	var LEDIntensity = 
+		(document.forms.SourceForm.AllSourceVoltage.value - document.forms.LEDForm.LEDForwardVoltage.value) / 
+		document.forms.RegisterForm.RegisterValue.value * 1000;
+	var target = document.getElementById("LEDIntensity");
+	target.value = LEDIntensity;
 }
