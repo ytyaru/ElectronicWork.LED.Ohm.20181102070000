490e46b25b3869d1ebfb745f0b024eda9a089299
ytyaru <ytyaru@outlook.jp>
2018-11-03 11:51:48 +0900

    抵抗器の算出を追加

--- docs/setup.js
+++ docs/setup.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
 	SetupSourceForm();
+	SetupRegisterForm();
 });
 function SetupSourceForm() {
 	document.forms.SourceForm.SourceVoltage.focus();
@@ -21,4 +22,24 @@ function CalcAllSourceVoltage() {
 		document.forms.SourceForm.SourceNumber.value;
 	var target = document.getElementById("AllSourceVoltage");
 	target.value = allSourceVoltage;
+}
+function SetupRegisterForm() {
+	var ro = document.getElementById("RegisterOhm");
+	ro.addEventListener("change", CalcRegisterValue);
+	ro.addEventListener("keyup", CalcRegisterValue);
+	ro.addEventListener("paste", CalcRegisterValue);
+	ro.addEventListener("input", CalcRegisterValue);
+	var re = document.getElementById("RegisterExponential");
+	re.addEventListener("change", CalcRegisterValue);
+	re.addEventListener("keyup", CalcRegisterValue);
+	re.addEventListener("paste", CalcRegisterValue);
+	re.addEventListener("input", CalcRegisterValue);
+	CalcRegisterValue();
+}
+function CalcRegisterValue() {
+	var RegisterValue = 
+		document.forms.RegisterForm.RegisterOhm.value * 
+		( 10 ** document.forms.RegisterForm.RegisterExponential.value );
+	var target = document.getElementById("RegisterValue");
+	target.value = RegisterValue;
 }
